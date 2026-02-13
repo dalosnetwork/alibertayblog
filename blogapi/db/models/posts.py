c5eb27eeae73638ea3df7d41f54cce69da6f0067
Dalos Network <125082532+dalosnetwork@users.noreply.github.com>
2026-02-13 10:13:26 +0300

    Delete blogapi directory

--- blogapi/db/models/posts.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text
-from ..database import Base
-
-class BlogPost(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    content = Column(Text)
