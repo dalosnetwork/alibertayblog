ea4868350ef7560537e4e65d92a68fe5a830ab4c
Dalos Network <125082532+dalosnetwork@users.noreply.github.com>
2026-02-13 10:13:17 +0300

    Delete blog directory

--- blog/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Navbar = () => (
-  <nav className="navbar">
-    Arise you have nothing to lose but your barbed wire fences
-  </nav>
-);
-
-export default Navbar;
